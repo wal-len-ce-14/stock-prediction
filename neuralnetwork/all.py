84a1bb275b219feb8e83678d6d20193e08089aab
wallence <a33369097@gmail.com>
2023-11-22 01:05:50 +0800

    add BBand feature

--- neuralnetwork/all.py
+++ neuralnetwork/all.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 
-# ss = twstock.Stock('2303')
-# print((ss.fetch(2023,3)))
+ss = twstock.Stock('2303')
+print((ss.fetch(2023,3)))
 
 
-aa = []
+# aa = []
 # aa = np.append(aa, [[]],axis=1)
 
-aa = [1,2]
-print(aa)
+# aa = [1,2]
+# print(aa)
 # print(np.append(aa, [[9,8,7,6,5,4,3,2,1]], axis=0))
 # print(np.append(aa, [[8,7]]))
 
@@ -34,3 +34,21 @@ print(aa)
 # data[:,1] = normalized
 # print(mean, std_value, normalized)
 
+# import numpy as np
+# import pandas as pd
+
+# # 假设 self.stock 是一个 Pandas DataFrame，包含 'close' 列
+# horizon = 20
+# stock = pd.DataFrame(          # get stock data
+#             [[1,2,3],[4,5,6],[7,8,9],[1,2,3],[4,5,6],[7,8,9],[1,2,3],[4,5,6],[7,8,9],[1,2,3]], 
+#             columns=['idx', 'close', 'num'],
+#             dtype=float
+#         )
+# # 计算滚动窗口均值和标准差
+# rolling_mean = stock.rolling(3).mean()
+# rolling_std = stock.rolling(3).std()
+
+# # 使用 numpy 进行计算，确保形状兼容
+# BBand_up = rolling_mean + 2 * np.array(rolling_std)
+# print(stock.rolling(3).std(), stock.rolling(3).mean())
+# print(stock)
--- randomforest/setup.py
+++ randomforest/setup.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import time
 
+
 # {
 #   2330台積電, 2344華邦電, 2303聯電, 2388威盛, 2402毅嘉, 3035智原, 2618長榮航, 2313華通, 
 #   3037欣興, 2883開發金, 2882國泰金,
@@ -11,39 +12,33 @@ import time
 # tws.__update_codes()
 
 def get_stock_history(stockid, start_year, end_year):
-        if start_year >= end_year:
+        if start_year >= end_year+1:
             print("[-] start_year >= end_year!!")
             return -1
         colume = [
-            'capacity', 
-            'open',
-            'high',
-            'low',
             'close',
+            'capacity',
             'change',
             'transaction'
         ]
         info = [[]]
         stock = tws.Stock(f"{stockid}")
-        for year in range(start_year, end_year):
+        for year in range(start_year, end_year+1):
             print(f"get stock info from {year} ...")
-            time.sleep(0.3)
+            time.sleep(1)
             for month in range(1, 13):
                 stock_infos = stock.fetch(year,month)
                 for stock_info in stock_infos:
-                    new_info = np.array([[    
-                        stock_info.capacity, 
-                        stock_info.open,
-                        stock_info.high,
-                        stock_info.low,
+                    new_info = np.array([[  
                         stock_info.close,
+                        stock_info.capacity, 
                         stock_info.change,
                         stock_info.transaction]])
                     if month == 1 and year == start_year:
                         info = new_info
                     else:
                         info = np.append(info, new_info, axis=0)
-        print(f"get stock info from {year+1} ...")
+        # print(f"get stock info from {year+1} ...")
         stockpd = pd.DataFrame(          # get stock data
             info, 
             columns=colume,
@@ -52,11 +47,11 @@ def get_stock_history(stockid, start_year, end_year):
         return stockpd
 
 class STOCK():
-    def __init__(self, stockid, start_year, end_year):
+    def __init__(self, stockid, start_year, end_year=2023):
         self.stock = get_stock_history(stockid, start_year, end_year)
         self.prodictors = [
-            'capacity', 
             'close',
+            'capacity', 
             'change',
             'transaction'
         ]
@@ -87,7 +82,7 @@ class STOCK():
         self.stock['tomorrow_change'] = self.stock['tomorrow_change'].apply(lambda x: format(x, ".2%"))
 
     def add_moving_average_info(self):
-        horizons = [2,5,20,60,120,240]
+        horizons = [2,5,20,60, 120, 240]
         new_predictor = []
 
         for horizon in horizons:
@@ -98,8 +93,29 @@ class STOCK():
             # 前幾天的上漲天數總和 因為trend不能包含到今天的資訊 要在shift(1)
             self.stock[trend_column] = self.stock.shift(1).rolling(horizon)["target"].sum() 
             new_predictor += [rolling_avg_column, trend_column]
-        self.stock = self.stock.dropna()
-        return new_predictor
+        self.prodictors += new_predictor
+        return self.prodictors
+
+    def add_BBands_info(self):
+        horizons = [20, 60, 120, 240]
+        new_predictor = []
+
+        for horizon in horizons:
+            rolling_std = (self.stock['close'].rolling(horizon)).std(ddof=1)
+            std_col = f"std_{horizon}"
+            self.stock[std_col] = rolling_std
+
+            mean = np.array(self.stock['close'].rolling(horizon).mean())
+            BBand_up = mean + rolling_std*2
+            BBand_down = mean - rolling_std*2
+            std_up_col = f"BBand_up_{horizon}"
+            std_down_col = f"BBand_down_{horizon}"
+            self.stock[std_up_col] = BBand_up
+            self.stock[std_down_col] = BBand_down
+
+            new_predictor += [std_col, std_up_col, std_down_col]
+        self.prodictors += new_predictor
+        return self.prodictors
 
     def Forest_model(self, split, n_estimators=500, min_samples_split=50, random_state=1):
         from sklearn.ensemble import RandomForestClassifier
@@ -124,39 +140,65 @@ class STOCK():
         precision = precision_score(test["target"], preds, average='micro')
         accuracy = accuracy_score(test["target"], preds)
         print('#########\n')
-        print('accuracy', accuracy)
+        print(f'accuracy {accuracy:.4f}')
         return [model, precision, accuracy]
 
-stock = STOCK(3037, 2010, 2023)
-stock.stock = stock.stock.dropna()
-print("###### pri_vol ######")
-stock.add_target_info()
-pc_vol_best = 0
-pc_vol_best_t = []
-for n in range(100, 1000, 100):
-    for min in range(10, 100, 10):
-        acc = stock.Forest_model(split=100,n_estimators=n, min_samples_split=min)[2]
-        if acc > pc_vol_best:
-            pc_vol_best = acc
-            pc_vol_best_t = [n,min]
-        print()
+    def drop_Nan(self):
+        self.stock = self.stock.dropna() 
+    
+    def to_test(self):
+        print(self.stock.tail(1))
+        return self.stock.tail(5)
 
-print("###### avg ######")
-stock.prodictors += stock.add_moving_average_info()
-avg_best = 0
-avg_best_t = []
-for n in range(100, 1000, 100):
-    for min in range(10, 100, 10):
-        acc = stock.Forest_model(split=100,n_estimators=n, min_samples_split=min)[2]
-        if acc > avg_best:
-            avg_best = acc
-            avg_best_t = [n,min]
-        print()
-print("######################\n")
-print('pc_vol_best', pc_vol_best)
-print('pc_vol_best_t', pc_vol_best_t)
-print('avg_best', avg_best)
-print('avg_best_t', avg_best_t)
+
+stock = STOCK(3037, 2022)
+stock.add_target_info()
+
+stock.drop_Nan()
+stock.add_moving_average_info()
+stock.add_BBands_info()
+stock.drop_Nan()
+# model = stock.Forest_model(split=200, n_estimators=200, min_samples_split=90)
+
+import joblib
+# joblib.dump(model, 'model')
+
+
+
+loaded_model = joblib.load('model')
+print(stock.to_test().iloc[1:])
+# result = loaded_model.predict(stock.to_test().iloc[0].values)
+# print(result)
+
+# stock.stock = stock.stock.dropna()
+# print("###### pri_vol ######")
+# stock.add_target_info()
+# pc_vol_best = 0
+# pc_vol_best_t = []
+# for n in range(100, 1000, 100):
+#     for min in range(10, 100, 10):
+#         acc = stock.Forest_model(split=100,n_estimators=n, min_samples_split=min)[2]
+#         if acc > pc_vol_best:
+#             pc_vol_best = acc
+#             pc_vol_best_t = [n,min]
+#         print()
+
+# print("###### avg ######")
+# stock.prodictors += stock.add_moving_average_info()
+# avg_best = 0
+# avg_best_t = []
+# for n in range(100, 1000, 100):
+#     for min in range(10, 100, 10):
+#         acc = stock.Forest_model(split=100,n_estimators=n, min_samples_split=min)[2]
+#         if acc > avg_best:
+#             avg_best = acc
+#             avg_best_t = [n,min]
+#         print()
+# print("######################\n")
+# print('pc_vol_best', pc_vol_best)
+# print('pc_vol_best_t', pc_vol_best_t)
+# print('avg_best', avg_best)
+# print('avg_best_t', avg_best_t)
 
 
 
